9a755fb2e85ddc4ffdc547858ae095e9e74377df
Ivan Dimitrov <ivan.v.dimitrov@pm.me>
2025-12-03 21:05:17 +0200

    add 2025 day 3.1

---
+++ 2025/03/solution.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+
+	"github.com/vanjiii/aoc/pkg"
+)
+
+func main() {
+	rows, err := pkg.ReadStrings("input")
+	if err != nil {
+		log.Fatalf("fail to read input: %v", err)
+		return
+	}
+
+	fmt.Println(partone(rows))
+}
+
+func partone(rows []string) int {
+	res := 0
+	for _, row := range rows {
+		idx := 0
+		maxi := byte('0')
+		for i := range len(row) - 1 {
+			if maxi < row[i] {
+				idx = i
+				maxi = row[i]
+			}
+		}
+
+		maxy := byte('0')
+		for i := idx + 1; i < len(row); i++ {
+			if maxy < row[i] {
+				idx = i
+				maxy = row[i]
+			}
+		}
+
+		tmp, _ := strconv.Atoi(string([]byte{maxi, maxy}))
+		// fmt.Println(tmp)
+		res += tmp
+	}
+
+	return res
+}
