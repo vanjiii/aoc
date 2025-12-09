92ac054018733ef9a77218c08e2cc44268c3481b
Ivan Dimitrov <ivan.v.dimitrov@pm.me>
2025-12-09 22:51:53 +0200

    add 2025 day 4.2

--- 2025/04/solution.go
+++ 2025/04/solution.go
@@ -8,18 +8,48 @@ import (
 )
 
 func main() {
-	grid, err := pkg.ReadGrid("input")
+	grid, err := pkg.ReadGrid("sample")
 	if err != nil {
 		log.Fatalf("fail to read input: %v", err)
 		return
 	}
 
-	fmt.Println(partone(grid))
+	res := 0
+	gr := grid
+
+	for {
+		// printm(gr)
+		curr := 0
+		curr, gr = partone(gr)
+		if 0 != curr {
+			res += curr
+		} else {
+			break
+		}
+	}
+
+	fmt.Println(res)
 }
 
-func partone(grid [][]rune) int {
+func printm(m [][]rune) {
+	fmt.Println("<<< EOT")
+	for row := range len(m) {
+		for col := range len(m[0]) {
+			fmt.Printf("%v ", string(m[row][col]))
+		}
+		fmt.Println()
+	}
+	fmt.Println("EOT")
+}
+
+func partone(grid [][]rune) (int, [][]rune) {
 	lrow := len(grid)
 	lcol := len(grid[0])
+
+	matrix := make([][]rune, lrow)
+	for i := range matrix {
+		matrix[i] = make([]rune, lcol)
+	}
 
 	res := 0
 
@@ -28,45 +58,66 @@ func partone(grid [][]rune) int {
 		for col := range lcol {
 			target := grid[row][col]
 
-			if target == '.' {
+			matrix[row][col] = target
+			if target == '.' || target == 'x' {
 				continue
 			}
+
+			rows := []int{}
+			cols := []int{}
 
 			neighbours := 0
 
 			if row-1 >= 0 && grid[row-1][col] == '@' {
+				rows = append(rows, row-1)
+				cols = append(cols, col)
 				neighbours++
 			}
 			if row+1 < lrow && grid[row+1][col] == '@' {
+				rows = append(rows, row+1)
+				cols = append(cols, col)
 				neighbours++
 			}
 
 			if row-1 >= 0 && col-1 >= 0 && grid[row-1][col-1] == '@' {
+				rows = append(rows, row-1)
+				cols = append(cols, col-1)
 				neighbours++
 			}
 			if col-1 >= 0 && grid[row][col-1] == '@' {
+				rows = append(rows, row)
+				cols = append(cols, col-1)
 				neighbours++
 			}
 			if row+1 < lrow && col-1 >= 0 && grid[row+1][col-1] == '@' {
+				rows = append(rows, row+1)
+				cols = append(cols, col-1)
 				neighbours++
 			}
 
 			if row-1 >= 0 && col+1 < lcol && grid[row-1][col+1] == '@' {
+				rows = append(rows, row-1)
+				cols = append(cols, col+1)
 				neighbours++
 			}
 			if col+1 < lcol && grid[row][col+1] == '@' {
+				rows = append(rows, row)
+				cols = append(cols, col+1)
 				neighbours++
 			}
 			if row+1 < lrow && col+1 < lcol && grid[row+1][col+1] == '@' {
+				rows = append(rows, row+1)
+				cols = append(cols, col+1)
 				neighbours++
 			}
 
 			if neighbours < treshhold {
 				res++
+				matrix[row][col] = 'x'
 			}
 
 		}
 	}
 
-	return res
+	return res, matrix
 }
